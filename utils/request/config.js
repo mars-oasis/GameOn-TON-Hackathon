8a86e9cff53d3666f1f046b302219b6cd5623b07
Nathaniel-byte <nathaniel00906@gmail.com>
2024-11-12 16:07:53 +0800

    Update config.js

--- utils/request/config.js
+++ utils/request/config.js
@@ -8,18 +8,17 @@ const env = accountInfo.miniProgram.envVersion
 // #ifndef MP-WEIXIN
 const env = 'release'
 // #endif
-// 这里的环境更改针对开发版和体验版 proApi, devApi, preApi
 const baseApi = devApi
 
 const CONFIG = {
-	//开发环境
+
 	development: {
 		baseUrl: baseApi[env],
 		formHeader: {
 			'Content-Type': 'application/x-www-form-urlencoded'
 		},
 	},
-	//生产环境配置
+
 	production: {
 		baseUrl: baseApi[env],
 		formHeader: {
