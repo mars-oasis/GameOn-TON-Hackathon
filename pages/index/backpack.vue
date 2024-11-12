e47e3c85fe99685d1c5fff05905e37d5b1f60244
Nathaniel-byte <nathaniel00906@gmail.com>
2024-11-12 15:44:02 +0800

    Update backpack.vue

--- pages/index/backpack.vue
+++ pages/index/backpack.vue
@@ -12,17 +12,17 @@
 							<text v-if="item.good_type === 1">{{ item.use_count }} / {{item.all_count}} </text>
 						</view>
 						<!-- <view v-if="item.good_type != 1"  class="tip">
-							<view v-if="Number(item.status) === 0" class="color_white color_bg_warning font_size_24 p_l_r_15 p_t_b_10">待使用</view>
-							<view v-if="Number(item.status) === 1" class="color_white color_bg_main font_size_24 p_l_r_15 p_t_b_10">使用中</view>
+							<view v-if="Number(item.status) === 0" class="color_white color_bg_warning font_size_24 p_l_r_15 p_t_b_10">to be used</view>
+							<view v-if="Number(item.status) === 1" class="color_white color_bg_main font_size_24 p_l_r_15 p_t_b_10">using</view>
 						</view> -->
 					</view>
 					<blockquote v-if="item.good_type === 1">
-						<view v-if="item.use_count >= item.all_count" class="m_t_15 color_bg_999 color_white text_align_cen p_t_b_10">没有可使用</view>
-						<view v-else class="m_t_15 color_bg_main color_white text_align_cen p_t_b_10" @click="handleDress(item)">使用</view>
+						<view v-if="item.use_count >= item.all_count" class="m_t_15 color_bg_999 color_white text_align_cen p_t_b_10">none</view>
+						<view v-else class="m_t_15 color_bg_main color_white text_align_cen p_t_b_10" @click="handleDress(item)">use</view>
 					</blockquote>
 					<blockquote v-else>
-						<view v-if="Number(item.status) === 0"  class="m_t_15 color_bg_main color_white text_align_cen p_t_b_10" @click="handleDress(item)">装扮</view>
-						<view v-if="Number(item.status) === 1" class="m_t_15 color_bg_999 color_white text_align_cen p_t_b_10" @click="handleDress(item)">取消装扮</view>
+						<view v-if="Number(item.status) === 0"  class="m_t_15 color_bg_main color_white text_align_cen p_t_b_10" @click="handleDress(item)">decorate</view>
+						<view v-if="Number(item.status) === 1" class="m_t_15 color_bg_999 color_white text_align_cen p_t_b_10" @click="handleDress(item)">cancel</view>
 					</blockquote>
 				</view>
 			</view>
@@ -30,12 +30,12 @@
 		<!-- v-if="popupShow" -->
 		<uni-popup ref="popupRef" type="center" :animation="true"> 
 			<view class="flex-col-center p_30" style="width:500rpx; background-color: #fff;box-sizing:border-box;border-radius: 20rpx">
-				<view class="">提示</view>
+				<view class="">tip</view>
 				<view class="flex_center flex p_t_20">
-					<view class="border p_t_b_10 p_l_r_30 m_r_30" @click="close">取消</view>						
-					<view v-if="detail.good_type === 1" class="color_bg_main color_white p_t_b_10 p_l_r_30" @click="handleUse">使用</view>
+					<view class="border p_t_b_10 p_l_r_30 m_r_30" @click="close">cancel</view>						
+					<view v-if="detail.good_type === 1" class="color_bg_main color_white p_t_b_10 p_l_r_30" @click="handleUse">use</view>
 					<blockquote v-else>
-						<view class="color_bg_main color_white p_t_b_10 p_l_r_30" @click="handleUse">{{ detail.status === 1 ? '取消装扮': '装扮'}}</view>
+						<view class="color_bg_main color_white p_t_b_10 p_l_r_30" @click="handleUse">{{ detail.status === 1 ? 'cancel': 'decorate'}}</view>
 					</blockquote>
 				</view>
 			</view>
@@ -56,19 +56,19 @@
 	const current = ref(0)
 	const detail = ref({})
 	const imgPath = config.IMG_PREFIX
-	const treeData = ref([]) // 渲染数据
-	const trees = ref({}) // 所有的数
+	const treeData = ref([])
+	const trees = ref({}) 
 	const tabs = reactive([
-		{index: 1,name: '道具', type: 1},
-		{index: 3,name: '天空', type:3},
-		{index: 4,name: '皮肤', type:4},
-		{index: 5,name: '挂饰', type: 5},
-		{index: 6,name: '宠物', type: 6},
+		{index: 1,name: 'Trees', type: 1},
+		{index: 3,name: 'Backgrounds', type:3},
+		{index: 4,name: 'Skins', type:4},
+		{index: 5,name: 'Ornament', type: 5},
+		{index: 6,name: 'Pets', type: 6},
 	])
 	const userInfo = computed(() => store.info)
 	const storeGoodsData = computed(() => store.storeGoodsList)
 	
-	// 配备图片和名称
+
 	const matchValue = (good_id, type) => {
 		if (type === 1) {
 			return storeGoodsData.value[good_id].name
@@ -76,12 +76,12 @@
 			return imgPath + storeGoodsData.value[good_id].img_url
 		}
 	}
-	// 切换tabs
+
 	const handleTabs =(index) => {
 		current.value = index
 		treeData.value = trees.value[tabs[current.value].type]
 	}
-	// 初始化数据
+
 	const getInitData = () => {
 		getGoodsData({
 			user_id: userInfo.value.user_id,
@@ -93,7 +93,7 @@
 	}
 	
 	const handleUse = () => {
-		// is_cancel:(可选)是否取消装扮，good_type为3~6的才需要该字断，传1是取消装扮，默认为0是使用装
+	
 		useGoods({
 			good_id: detail.value.good_id,
 			user_id: userInfo.value.user_id,
@@ -104,7 +104,7 @@
 				title: res.data,
 				icon:'none'
 			})
-			// 重新刷新数据
+			
 			getInitData()
 			store.setHomedata({ user_id: userInfo.value.user_id })
 			setTimeout(() => {
@@ -113,13 +113,13 @@
 		})
 	}
 	
-	// 装扮
+	
 	const handleDress = (item) => {
 		if (item.good_type === 1) {
 			if (item.all_count <= item.use_count) return
 		} 
 		// else{
-		// 	if(item.status === 1) return // 3-6中的商品已装扮的需要取消装扮
+		// 	if(item.status === 1) return 
 		// }
 		popupShow.value = true
 		detail.value = item
