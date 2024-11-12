09df0a17788bebe26aaf964426b2f106e7e7ea33
Nathaniel-byte <nathaniel00906@gmail.com>
2024-11-12 19:10:04 +0800

    Update request_config.js

--- config/request_config.js
+++ config/request_config.js
@@ -1,14 +1,6 @@
 // const DEV = 'apiPrefix'
 // const PRE = 'apiPrefix'
 // const PRO = 'apiPrefix'
-
-const DEV = "https://ton.robot68.online"
-const PRE = "https://ton.robot68.online"
-const PRO = "https://ton.robot68.online"
-
-// const DEV = "http://127.0.0.1:25000"
-// const PRE = "http://127.0.0.1:25000"
-// const PRO = "http://127.0.0.1:25000"
 export default {
 	
 	devApi: {
