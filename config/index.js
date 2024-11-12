db448ab3ff0b412586e7ab65090f598d4245b603
Nathaniel-byte <nathaniel00906@gmail.com>
2024-11-12 19:09:49 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,9 +1,6 @@
 export default {
 	
-	MAP_KEY: '582a3270daaead758316ed31d3646af3',
 	
 	COS_IMG_PAHT: '',
 	
-	IMG_PREFIX: 'https://ton.robot68.online',
-	// IMG_PREFIX: 'http://8.211.5.38:25003',
 }
